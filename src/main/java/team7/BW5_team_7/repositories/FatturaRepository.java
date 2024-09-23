cbca3e467b7a296e52f4ecdc76b583cda704aa3e
Mario Fragnito <mariofragnitoph@gmail.com>
2024-09-23 17:28:20 +0200

    Fixed package issue

--- src/main/java/team7/BW5_team_7/repositories/FatturaRepository.java
+++ src/main/java/team7/BW5_team_7/repositories/FatturaRepository.java
@@ -1,4 +1,4 @@
-package team7.BW5_team_7.repositorys;
+package team7.BW5_team_7.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/team7/BW5_team_7/services/FatturaService.java
+++ src/main/java/team7/BW5_team_7/services/FatturaService.java
@@ -2,7 +2,7 @@ package team7.BW5_team_7.services;
 
 
 import org.springframework.stereotype.Service;
-import team7.BW5_team_7.repositorys.FatturaRepository;
+import team7.BW5_team_7.repositories.FatturaRepository;
 
 @Service
 public class FatturaService {
