c06b25d50305696a7ebfbbf81c295c58b03c8228
Mario Fragnito <mariofragnitoph@gmail.com>
2024-09-23 11:38:22 +0200

    Adds initial setup

---
+++ src/main/java/team7/BW5_team_7/Bw5Team7Application.java
@@ -0,0 +1,13 @@
+package team7.BW5_team_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bw5Team7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bw5Team7Application.class, args);
+	}
+
+}
---
+++ src/test/java/team7/BW5_team_7/Bw5Team7ApplicationTests.java
@@ -0,0 +1,13 @@
+package team7.BW5_team_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bw5Team7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
