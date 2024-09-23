4e6cec123cfc2cb15cf9f197105415f4516d76d6
Yuri Impellizzeri <128586274+yvr1x02@users.noreply.github.com>
2024-09-23 13:52:27 +0200

    Create fatture.java
    Creazione classe Fatture

---
+++ src/main/java/team7/BW5_team_7/entities/fatture.java
@@ -0,0 +1,38 @@
+package team7.BW5_team_7.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "fatture")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+
+public class fatture {
+    @Id
+    @GeneratedValue
+    private UUID idFattura;
+    private Date data;
+    private double importo;
+    private int numeroFattura;
+
+    public fatture(UUID idFattura, Date data, double importo, int numeroFattura) {
+        this.idFattura = idFattura;
+        this.data = data;
+        this.importo = importo;
+        this.numeroFattura = numeroFattura;
+    }
+}
+
