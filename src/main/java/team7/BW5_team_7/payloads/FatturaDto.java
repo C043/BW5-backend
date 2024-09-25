40e57fbfbe8dc324a67773d88713ff517025c4b6
C043 <mariofragnitoph@gmail.com>
2024-09-25 15:22:45 +0200

    Revert "Added FatturaDto"
    This reverts commit 2d644c91fe62bfa55c2e1a8a0bad5951426a3fdd.

--- src/main/java/team7/BW5_team_7/payloads/FatturaDto.java
+++ src/main/java/team7/BW5_team_7/payloads/FatturaDto.java
@@ -1,25 +1,4 @@
 package team7.BW5_team_7.payloads;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import team7.BW5_team_7.entities.Cliente;
-
-import java.time.LocalDate;
-
-public record FatturaDto(
-        @NotNull(message = "Il Campo Data è obbligatorio!")
-        LocalDate data,
-
-        @Positive(message = "L'importo deve essere un valore positivo!")
-        double importo,
-
-        @NotEmpty(message = "Il Campo Numero è obbligatorio!")
-        String numero,
-
-        @NotEmpty(message = "Il Campo Stato è obbligatorio!")
-        String stato,
-
-        @NotNull(message = "Il Campo Cliente è obbligatorio!")
-        Cliente cliente) {
+public record FatturaDto() {
 }
--- src/main/java/team7/BW5_team_7/services/FatturaService.java
+++ src/main/java/team7/BW5_team_7/services/FatturaService.java
@@ -1,15 +1,10 @@
 package team7.BW5_team_7.services;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import team7.BW5_team_7.repositories.FatturaRepository;
-
-import java.util.List;
 
 @Service
 public class FatturaService {
     private FatturaRepository fatturaRepository;
-
-
-
+}
