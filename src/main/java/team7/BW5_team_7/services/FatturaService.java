8b582c5a65079ce3b5f23033fae427bafcbaa9fd
C043 <mariofragnitoph@gmail.com>
2024-09-25 15:28:58 +0200

    Revert "Revert "Fixed one filter""
    This reverts commit e753702b8de4e395227c03234d7e32f90d95ccd9.

--- src/main/java/team7/BW5_team_7/services/FatturaService.java
+++ src/main/java/team7/BW5_team_7/services/FatturaService.java
@@ -3,6 +3,7 @@ package team7.BW5_team_7.services;
 
 import org.springframework.stereotype.Service;
 import team7.BW5_team_7.entities.Fattura;
+import team7.BW5_team_7.entities.StatoFattura;
 import team7.BW5_team_7.repositories.FatturaRepository;
 
 import java.time.LocalDate;
@@ -12,6 +13,9 @@ import java.util.UUID;
 @Service
 public class FatturaService {
     private FatturaRepository fatturaRepository;
+
+    @Autowired
+    private StatoFatturaService statoFatturaService;
 
     public List<Fattura> getAllFatture() {
         return fatturaRepository.findAll();
@@ -48,8 +52,9 @@ public class FatturaService {
     }
 
     // Filtra le fatture per stato
-    public List<Fattura> filterByStato(String statoFattura) {
-        return fatturaRepository.findByStatoFattura(statoFattura);
+    public List<Fattura> filterByStato(String nome) {
+        StatoFattura found = this.statoFatturaService.findByNome(nome);
+        return fatturaRepository.findByStatoFattura(found);
     }
 
     // Filtra le fatture per data
