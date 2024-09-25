d921bbc4682f933e48c055d5a7d55d4692563fd8
C043 <mariofragnitoph@gmail.com>
2024-09-25 15:28:58 +0200

    Revert "Revert "Revert "Merge branch 'feature/filtriClienti' into develop"""
    This reverts commit 0b8b85dada990531a1a3011ca1cd47ebb05f69f1.

--- src/main/java/team7/BW5_team_7/repositories/ClientiRepository.java
+++ src/main/java/team7/BW5_team_7/repositories/ClientiRepository.java
@@ -1,24 +1,12 @@
 package team7.BW5_team_7.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import team7.BW5_team_7.entities.Cliente;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ClientiRepository extends JpaRepository<Cliente, UUID> {
     boolean existsByEmail(String email);
-
-    @Query("SELECT c FROM Cliente c WHERE c.fatturatoAnnuale >= :minimo")
-    List<Cliente> filterByMoreOrEqualFatturatoAnnuo(double minimo);
-
-    List<Cliente> findByDataInserimento(LocalDate dataInserimento);
-
-    List<Cliente> findByDataUltimoContatto(LocalDate data);
-
-    List<Cliente> findByRagioneSocialeContainsIgnoreCase(String query);
 }
