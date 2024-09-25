2d644c91fe62bfa55c2e1a8a0bad5951426a3fdd
Yuri Impellizzeri <128586274+yvr1x02@users.noreply.github.com>
2024-09-25 09:20:15 +0200

    Added FatturaDto

--- src/main/java/team7/BW5_team_7/services/FatturaService.java
+++ src/main/java/team7/BW5_team_7/services/FatturaService.java
@@ -1,10 +1,17 @@
 package team7.BW5_team_7.services;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import team7.BW5_team_7.repositories.FatturaRepository;
 
+import java.util.List;
+
 @Service
 public class FatturaService {
+
+    @Autowired
     private FatturaRepository fatturaRepository;
-}
+
+
+
